4cbcde7eec066a6c36cc596a3a4deadbe858dce1
JCROIZIER <jules.croizier@ynov.com>
2021-12-07 09:33:31 +0100

    reccuperation_lettre

---
+++ hangman-web/serveur.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	tmpl, err := template.ParseFiles("./templates/index.gohtml")
+	if err != nil {
+		fmt.Println("erreur ouverture template")
+		return
+	}
+	type Page struct {
+		Title   string
+		Content string
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+		fmt.Println(r.Header.Get("User-Agent"))
+		data := Page{"Titre 1 ", "Contenu"}
+		tmpl.ExecuteTemplate(w, "index", data)
+		if r.Method == "POST" {
+			lettre := r.FormValue("letter") //recupere la valeur letter du formulaire ( html)
+			fmt.Println(lettre)
+		} else if r.Method == "GET" {
+			fmt.Println("pas de POST effectué")
+		}
+	})
+
+	fileServer := http.FileServer(http.Dir("assets"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	http.ListenAndServe("localhost:3000", nil)
+}
+
+/*func recuperlalettrepoto() {
+	lettre := r.FormValue("letter")
+}*/
+
+//TODO faire 3 partie pour chaque demande dans la base de donnée, une partiesi c'est GET , une autre si c'est POST et une autre qui differe des autres parties ( != GET || Post)
