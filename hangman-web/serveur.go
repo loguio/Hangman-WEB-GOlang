f529062c5eadfe513992a281a453e4f912ff2248
JCROIZIER <jules.croizier@ynov.com>
2021-12-09 09:43:22 +0100

    avec_valeur_initial

--- hangman-web/serveur.go
+++ hangman-web/serveur.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
+
+	hangman "./hangman-classic"
+	game "./hangman-classic/test"
 )
 
 func main() {
@@ -21,9 +25,15 @@ func main() {
 		fmt.Println(r.Header.Get("User-Agent"))
 		data := Page{"Titre 1 ", "Contenu"}
 		tmpl.ExecuteTemplate(w, "index", data)
+		game.Lettre = r.FormValue("letter")   //recupere la valeur letter du formulaire ( html)
+		InitString := hangman.GetRandomWord() //met chaque mot de la liste de mot dans un tableau de string
+		game.RandomWord = (InitString[rand.Intn(len(InitString))])
+		game.ArrayInit = hangman.InitArray(game.RandomWord)
+		game.ArrayAnswer = hangman.InitArray(game.RandomWord)
 		if r.Method == "POST" {
-			lettre := r.FormValue("letter") //recupere la valeur letter du formulaire ( html)
-			fmt.Println(lettre)
+			//numberOfAttemps := game.Game(game.Lettre)
+			//fmt.Println(numberOfAttemps)
+			fmt.Println(game.Lettre)
 		} else if r.Method == "GET" {
 			fmt.Println("pas de POST effectué")
 		}
@@ -35,8 +45,6 @@ func main() {
 	http.ListenAndServe("localhost:3000", nil)
 }
 
-/*func recuperlalettrepoto() {
-	lettre := r.FormValue("letter")
-}*/
-
 //TODO faire 3 partie pour chaque demande dans la base de donnée, une partiesi c'est GET , une autre si c'est POST et une autre qui differe des autres parties ( != GET || Post)
+//TODO Une fonction avec des switch pour l'état du pendu
+//TODO Image pour pendu
