6592af89910aee7f67f266799c0e4a2a0f5f6fe9
Marius_bourse <you@example.com>
2021-12-10 08:10:30 +0100

    last save

--- serveur.go
+++ serveur.go
@@ -16,18 +16,20 @@ func main() {
 		Title  string
 		Letter string
 	}
-	str := []string{"oui", "uwu", "aayay"}
+	//str := []string{""}
 	i := 0
+	list_letter := ""
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
 
 		fmt.Println(r.Header.Get("User-Agent"))
-		data := Page{"Hangman-Web ", str[i]}
+		data := Page{"Hangman-Web ", list_letter}
 
 		if r.Method == "POST" {
 			i++
 			lettre := r.FormValue("letter") //recupere la valeur letter du formulaire ( html)
-			str = append(str, lettre)
-			data = Page{"Hangman-Web ", str[i]}
+			list_letter += lettre
+			list_letter += ", "
+			data = Page{"Hangman-Web ", list_letter}
 			fmt.Println(lettre)
 		} else if r.Method == "GET" {
 			fmt.Println("pas de POST effectué")
