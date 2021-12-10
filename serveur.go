826a2dd7ee444167b7a7636b27371e465c4523de
Marius_bourse <you@example.com>
2021-12-10 09:22:40 +0100

    ajout de la gestion d'errreur 404 et 501

--- serveur.go
+++ serveur.go
@@ -13,43 +13,50 @@ import (
 func main() {
 	tmpl, err := template.ParseFiles("./templates/index.gohtml")
 	if err != nil {
-		fmt.Println("erreur ouverture template")
-		return
-	}
-	type Page struct {
-		Title  string
-		Letter string
-	}
-	InitString := hangman.GetRandomWord() //met chaque mot de la liste de mot dans un tableau de string
-	game.RandomWord = (InitString[rand.Intn(len(InitString))])
-	game.ArrayInit = []rune(game.RandomWord)
-	game.ArrayAnswer = hangman.InitArray(game.RandomWord)
-	game.NumberOfAttemps = 11
-	//str := []string{""}
-	i := 0
-	list_letter := ""
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+			tmpl, err = template.ParseFiles("./templates/error404.gohtml")
+			tmpl.ExecuteTemplate(w, "error404", nil)
+		})
+		http.ListenAndServe("localhost:3000", nil)
 
-		fmt.Println(r.Header.Get("User-Agent"))
-		data := Page{"Hangman-Web ", list_letter}
-
-		if r.Method == "POST" {
-			i++
-			lettre := r.FormValue("letter") //recupere la valeur letter du formulaire ( html)
-			list_letter += lettre
-			list_letter += ", "
-			data = Page{"Hangman-Web ", list_letter}
-			fmt.Println(lettre)
-		} else if r.Method == "GET" {
-			fmt.Println("pas de POST effectué")
+	} else {
+		type Page struct {
+			Title  string
+			Letter string
 		}
-		tmpl.ExecuteTemplate(w, "index", data)
-	})
+		InitString := hangman.GetRandomWord() //met chaque mot de la liste de mot dans un tableau de string
+		game.RandomWord = (InitString[rand.Intn(len(InitString))])
+		game.ArrayInit = []rune(game.RandomWord)
+		game.ArrayAnswer = hangman.InitArray(game.RandomWord)
+		game.NumberOfAttemps = 11
+		list_letter := ""
 
-	fileServer := http.FileServer(http.Dir("assets"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+			fmt.Println(r.Header.Get("User-Agent"))
+			data := Page{"Hangman-Web ", list_letter}
+			if r.Method == "POST" {
+				lettre := r.FormValue("letter") //recupere la valeur letter du formulaire ( html)
+				list_letter += lettre
+				list_letter += ", "
+				data = Page{"Hangman-Web ", list_letter}
+				fmt.Println(lettre)
+				tmpl.ExecuteTemplate(w, "index", data)
+			} else if r.Method == "GET" {
+				fmt.Println("pas de POST effectué")
+				tmpl.ExecuteTemplate(w, "index", data)
+			} else {
+				tmpl, err = template.ParseFiles("./templates/error501.gohtml")
+				tmpl.ExecuteTemplate(w, "error501", nil)
+			}
 
-	http.ListenAndServe("localhost:3000", nil)
+		})
+
+		fileServer := http.FileServer(http.Dir("assets"))
+		http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+		http.ListenAndServe("localhost:3000", nil)
+	}
+
 }
 
 //TODO faire 3 partie pour chaque demande dans la base de donnée, une partiesi c'est GET , une autre si c'est POST et une autre qui differe des autres parties ( != GET || Post)
