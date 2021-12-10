0e43d5e59977cd2341a8973870ff9e78ed80dd8b
JCROIZIER <jules.croizier@ynov.com>
2021-12-10 15:37:38 +0100

    correctif beug, a regler : victoire, lisibilité du code

--- serveur.go
+++ serveur.go
@@ -3,53 +3,73 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"math/rand"
 	"net/http"
 
-	hangman "./hangman-classic"
 	game "./hangman-classic/test"
 )
 
+var tabURL = []string{
+	"",
+	"https://i.goopics.net/bqidl2.png",
+	"https://i.goopics.net/rojgcs.png",
+	"https://i.goopics.net/igktwf.png",
+	"https://i.goopics.net/a8sgek.png",
+	"https://i.goopics.net/a9mnww.png",
+	"https://i.goopics.net/lif1mq.png",
+	"https://i.goopics.net/hy8no6.png",
+	"https://i.goopics.net/h377xl.png",
+	"https://i.goopics.net/rswcvd.png",
+	"https://i.goopics.net/uiwsjx.png"}
+
 func main() {
+	game.NumberOfAttemps = 10
 	tmpl, err := template.ParseFiles("./templates/index.gohtml")
 	if err != nil {
-		fmt.Println("erreur ouverture template")
-		return
-	}
-	type Page struct {
-		Title  string
-		Letter string
-	}
-	InitString := hangman.GetRandomWord() //met chaque mot de la liste de mot dans un tableau de string
-	game.RandomWord = (InitString[rand.Intn(len(InitString))])
-	game.ArrayInit = []rune(game.RandomWord)
-	game.ArrayAnswer = hangman.InitArray(game.RandomWord)
-	game.NumberOfAttemps = 11
-	//str := []string{""}
-	i := 0
-	list_letter := ""
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+			tmpl, err = template.ParseFiles("./templates/error404.gohtml")
+			tmpl.ExecuteTemplate(w, "error404", nil)
+		})
+		http.ListenAndServe("localhost:3000", nil)
 
-		fmt.Println(r.Header.Get("User-Agent"))
-		data := Page{"Hangman-Web ", list_letter}
-
-		if r.Method == "POST" {
-			i++
-			lettre := r.FormValue("letter") //recupere la valeur letter du formulaire ( html)
-			list_letter += lettre
-			list_letter += ", "
-			data = Page{"Hangman-Web ", list_letter}
-			fmt.Println(lettre)
-		} else if r.Method == "GET" {
-			fmt.Println("pas de POST effectué")
+	} else {
+		type Page struct {
+			Title           string
+			Letter          string
+			URLpendu        string
+			NumberOfAttemps int
+			SearchWord      string
 		}
-		tmpl.ExecuteTemplate(w, "index", data)
-	})
+		list_letter := ""
 
-	fileServer := http.FileServer(http.Dir("assets"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+			data := Page{"Hangman-Web ", list_letter, "", game.NumberOfAttemps, string(game.ArrayAnswer)}
+			if r.Method == "POST" {
+				game.Lettre = r.FormValue("letter") //recupere la valeur letter du formulaire (html)
+				game.Game()
+				if game.Lettre != "" && game.Lettre != " " {
+					list_letter += game.Lettre
+					list_letter += ", "
+				}
+				if game.NumberOfAttemps == 0 {
+					data = Page{"Hangman-Web ", list_letter, "https://th.bing.com/th/id/OIP.kq55Q_4YugKpMd67w_YD3wHaFO?pid=ImgDet&rs=1", game.NumberOfAttemps, string(game.ArrayAnswer)}
+				} else {
+					data = Page{"Hangman-Web ", list_letter, tabURL[game.NumberOfAttemps], game.NumberOfAttemps, string(game.ArrayAnswer)}
+				}
+				tmpl.ExecuteTemplate(w, "index", data)
+			} else if r.Method == "GET" {
+				fmt.Println("pas de POST effectué")
+				tmpl.ExecuteTemplate(w, "index", data)
+			} else {
+				tmpl, err = template.ParseFiles("./templates/error501.gohtml")
+				tmpl.ExecuteTemplate(w, "error501", nil)
+			}
 
-	http.ListenAndServe("localhost:3000", nil)
+		})
+
+		fileServer := http.FileServer(http.Dir("assets")) //application du CSS qui sera en fichier statique
+		http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+		fmt.Println("le serveur est en cours d'éxécution a l'adresse localhost:3000")
+		http.ListenAndServe("localhost:3000", nil)
+	}
 }
-
-//TODO faire 3 partie pour chaque demande dans la base de donnée, une partiesi c'est GET , une autre si c'est POST et une autre qui differe des autres parties ( != GET || Post)
