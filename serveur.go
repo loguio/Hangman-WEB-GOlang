ce65653f124f83b977e91d84e3cb92ed3362b686
JCROIZIER <jules.croizier@ynov.com>
2021-12-14 08:48:34 +0100

    1ere version finalisé a bien refaire au propre

--- serveur.go
+++ serveur.go
@@ -18,6 +18,7 @@ type Page struct {
 	SearchWord      string
 	Word            string
 	WordFind        bool
+	Tabletter       []string
 }
 
 var tabURL = []string{
@@ -34,6 +35,8 @@ var tabURL = []string{
 	"https://i.goopics.net/uiwsjx.png"}
 
 var WordFind bool
+var tabletter []string
+var list_letter string
 
 func main() {
 
@@ -47,38 +50,46 @@ func main() {
 }
 func website() {
 
-	game.NumberOfAttemps = 10
 	tmpl, err := template.ParseFiles("./templates/index.gohtml")
 	if err != nil {
 		Error404()
 	} else {
-
-		list_letter := ""
-
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
-			data := Page{"Hangman-Web ", list_letter, "", game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayAnswer), WordFind}
+			data := Page{"Hangman-Web ", list_letter, "", game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayAnswer), WordFind, tabletter}
 			if r.Method == "POST" {
 				if r.FormValue("restart") == "Restart" {
 					Restart()
+					data = Page{"Hangman-Web ", list_letter, "", game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayAnswer), WordFind, tabletter}
 				} else {
 					game.Lettre = r.FormValue("letter") //recupere la valeur letter du formulaire (html)
-					game.Game()
+					same := false
+					for i := 0; i < len(tabletter); i++ {
+						if game.Lettre == tabletter[i] {
+							same = true
+						}
+					}
+					if game.Lettre == "" {
+						fmt.Println(err)
+					}
 					if string(game.ArrayAnswer) == string(game.ArrayInit) {
 						WordFind = true
 					}
-					if game.Lettre != "" && game.Lettre != " " {
+					if game.Lettre != "" && game.Lettre != " " && !same {
+						tabletter = append(tabletter, game.Lettre)
 						list_letter += game.Lettre
 						list_letter += ", "
+						game.Game()
 					}
 					if game.NumberOfAttemps == 0 {
-						data = Page{"Hangman-Web ", list_letter, "https://th.bing.com/th/id/OIP.kq55Q_4YugKpMd67w_YD3wHaFO?pid=ImgDet&rs=1", game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayInit), WordFind}
+						data = Page{"Hangman-Web ", list_letter, "https://th.bing.com/th/id/OIP.kq55Q_4YugKpMd67w_YD3wHaFO?pid=ImgDet&rs=1", game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayInit), WordFind, tabletter}
 					} else {
-						data = Page{"Hangman-Web ", list_letter, tabURL[game.NumberOfAttemps], game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayInit), WordFind}
+						data = Page{"Hangman-Web ", list_letter, tabURL[game.NumberOfAttemps], game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayInit), WordFind, tabletter}
 					}
 				}
 				tmpl.ExecuteTemplate(w, "index", data)
 			} else if r.Method == "GET" {
-				fmt.Println("pas de POST effectué")
+				data := Page{"Hangman-Web ", list_letter, "", game.NumberOfAttemps, string(game.ArrayAnswer), string(game.ArrayAnswer), WordFind, tabletter}
+				fmt.Println("GET")
 				tmpl.ExecuteTemplate(w, "index", data)
 			} else {
 				Error501()
@@ -89,17 +100,18 @@ func website() {
 }
 
 func Restart() {
+	tabletter = nil
 	WordFind = false
 	game.InitString = hangman.GetRandomWord()
 	game.RandomWord = string(game.InitString[rand.Intn(len(game.InitString))])
-	game.ArrayAnswer = hangman.InitArray(game.RandomWord)
 	game.ArrayInit = []rune(game.RandomWord)
+	game.ArrayAnswer = hangman.InitArray(game.RandomWord)
 	game.NumberOfAttemps = 10
-
 }
 
+//fonction qui crée la page de l'erreur 404
 func Error404() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //crée une page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, _ := template.ParseFiles("./templates/error404.gohtml")
 		fmt.Println("le serveur est en cours d'éxécution à l'adresse localhost:3000")
 		tmpl.ExecuteTemplate(w, "error404", nil)
@@ -107,6 +119,8 @@ func Error404() {
 	http.ListenAndServe("localhost:3000", nil)
 
 }
+
+//fonction qui crée la page de l'erreur 501
 func Error501() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, _ := template.ParseFiles("./tempaltes/error501.gohtml")
