2b0bf6dd410176a4536d87fdd2db28682799790d
Marius_bourse <you@example.com>
2022-01-10 21:17:10 +0100

    synchro complete

--- hangman-classic/test/game.go
+++ hangman-classic/test/game.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"fmt"
 	"math/rand"
 
 	hangman ".."
@@ -13,28 +12,33 @@ var Lettre string
 var ArrayAnswer []rune = hangman.InitArray(RandomWord)
 var ArrayInit []rune = []rune(RandomWord)
 var NumberOfAttemps int = 10
+var LetterGoodFormat bool
+var WordFind = false
 
-func Game() {
+func Game() bool {
 	if InitString == nil { //si le fichier n'est pas trouvé et donc il n'y a pas de mot disponible pour le jeu, arret du jeu
-		fmt.Println("erreur obtention mot aléatoire")
-		return
+		return false
 	}
-	hangman.Compare(Lettre, ArrayInit, ArrayAnswer) //regarde si la lettre est contenu dans le mot
-	compare := hangman.Compare(Lettre, ArrayInit, ArrayAnswer)
-	if compare == false { //si la lettre n'est pas contenue
-		NumberOfAttemps--
-	} else if compare == true { //si la lettre est contenue dans le mot, remplacer dans le tableau de réponse le caractère _ par la lettre mise par le joueur
-		arrayRune := []rune(Lettre)
-		pos := []int(nil)
-		for i := 0; i < len(ArrayInit); i++ {
-			if arrayRune[0] == ArrayInit[i] {
-				pos = append(pos, i)
-			}
-			for i := 0; i < len(pos); i++ {
-				ArrayAnswer[pos[i]] = ArrayInit[pos[i]]
+	LetterGoodFormat = hangman.UserLetter(Lettre)
+	if LetterGoodFormat == true {
+		hangman.Compare(Lettre, ArrayInit, ArrayAnswer) //regarde si la lettre est contenu dans le mot
+		compare := hangman.Compare(Lettre, ArrayInit, ArrayAnswer)
+		if compare == false { //si la lettre n'est pas contenue
+			NumberOfAttemps--
+		} else if compare == true { //si la lettre est contenue dans le mot, remplacer dans le tableau de réponse le caractère _ par la lettre mise par le joueur
+			arrayRune := []rune(Lettre)
+			pos := []int(nil)
+			for i := 0; i < len(ArrayInit); i++ {
+				if arrayRune[0] == ArrayInit[i] {
+					pos = append(pos, i)
+				}
+				for i := 0; i < len(pos); i++ {
+					ArrayAnswer[pos[i]] = ArrayInit[pos[i]]
+				}
 			}
 		}
+	} else {
+		LetterGoodFormat = false
 	}
+	return true
 }
-
-//TODO renvoye le nb de vie actualisé
